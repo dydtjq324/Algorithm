d11f2beac680a266efba5ac8e27699ce5e0e60a6
Lee yong sub <62845007+dydtjq324@users.noreply.github.com>
2023-07-08 03:07:47 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,10 +1,12 @@
 function solution(s) {
     var answer = '';
-    if(s.length % 2 ==0){ //짝수이면
-        answer+=s[(s.length)/2 - 1]
-        answer+=s[(s.length)/2]
-    }else{
-        answer+=s[Math.floor((s.length)/2)]
+    if(s.length % 2 ==0){
+        
+        answer+=s[Math.floor(s.length/2)-1]
+        answer+=s[Math.floor(s.length/2)]
     }
+    else 
+        
+        answer+=s[Math.floor(s.length/2)]
     return answer;
 }
