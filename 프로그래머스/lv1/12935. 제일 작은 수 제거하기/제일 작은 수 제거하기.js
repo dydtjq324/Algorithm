68d15080cbe9130b2619d07c835c37068c1cda23
Lee yong sub <62845007+dydtjq324@users.noreply.github.com>
2023-07-08 02:52:48 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,5 +1,5 @@
-function solution(numbers) {
-    numbers.splice(numbers.indexOf(Math.min(...numbers)),1)
-     if(numbers.length===0)return -1
-     return numbers
+function solution(arr) {
+    arr.splice(arr.indexOf(Math.min(...arr)),1);
+    if(arr.length<1)return[-1];
+    return arr;
 }
