5b1a2fa1c273e28a02455c0af0485e4a17b9b00a
Lee yong sub <62845007+dydtjq324@users.noreply.github.com>
2023-07-10 01:58:34 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,9 @@
+function solution(price, money, count) {
+    var answer = 0;
+    for(let i=1; i<=count; i++){
+        answer+=price*i;
+    }
+    if(answer-money>0)answer=answer-money
+    else return 0
+    return answer
+}
